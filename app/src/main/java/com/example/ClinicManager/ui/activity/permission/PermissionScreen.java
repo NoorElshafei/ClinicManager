639644049bb52178c0f64489c0508582e5c604b8
Noor Elshafei <noor.elshafie@gmail.com>
2020-08-16 09:17:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ClinicManager/ui/activity/permission/PermissionScreen.java
@@ -0,0 +1,16 @@
+package com.example.ClinicManager.ui.activity.permission;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ClinicManager.R;
+
+public class PermissionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_permission_screen);
+    }
+}
