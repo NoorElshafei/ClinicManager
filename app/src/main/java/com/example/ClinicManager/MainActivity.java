0c4ba0b41026b08c531411fa00c63c9d4cd59d84
Noor Elshafei <noor.elshafie@gmail.com>
2020-08-17 18:26:05 +0200

    design update 2

--- app/src/main/java/com/example/ClinicManager/MainActivity.java
+++ app/src/main/java/com/example/ClinicManager/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
@@ -13,38 +15,78 @@ import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import com.example.ClinicManager.ui.fragment.appointment_list.AppointmentListFragment;
+import com.example.ClinicManager.ui.fragment.dashboard.DashBoardFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private DrawerLayout drawer;
+    private ImageView menu;
+    private LinearLayout appointmentMenuItem, dashBoardMenuItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar) ;
-        setSupportActionBar(toolbar) ;
 
-        drawer = findViewById(R.id.drawer_layout) ;
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer , toolbar , R.string. navigation_drawer_open ,
-                R.string. navigation_drawer_close ) ;
-        drawer.addDrawerListener(toggle) ;
-        toggle.syncState() ;
+        drawer = findViewById(R.id.drawer_layout);
+        menu = findViewById(R.id.menu_image);
+        appointmentMenuItem = findViewById(R.id.appointment_menu_item);
+        dashBoardMenuItem = findViewById(R.id.dashboard_menu_item);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, DashBoardFragment.newInstance(), "DashBoardFragment")
+                .commit();
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (getDrawer().isDrawerOpen(GravityCompat.START)) {
+                    getDrawer().closeDrawer(GravityCompat.START);
+                } else {
+                    getDrawer().openDrawer(GravityCompat.START);
+                }
+            }
+        });
+
+
+        dashBoardMenuItem.setOnClickListener(this);
+        appointmentMenuItem.setOnClickListener(this);
 
     }
+
     @Override
-    public void onBackPressed () {
-        DrawerLayout drawer = findViewById(R.id. drawer_layout ) ;
-        if (drawer.isDrawerOpen(GravityCompat. START )) {
-            drawer.closeDrawer(GravityCompat. START ) ;
+    public void onBackPressed() {
+
+        if (getDrawer().isDrawerOpen(GravityCompat.START)) {
+            getDrawer().closeDrawer(GravityCompat.START);
         } else {
-            super .onBackPressed() ;
+            super.onBackPressed();
         }
     }
 
+    public DrawerLayout getDrawer() {
+        return drawer;
+    }
 
+    @Override
+    public void onClick(View view) {
+        if (view == appointmentMenuItem) {
+            openFragment(new AppointmentListFragment());
+        } else if (view == dashBoardMenuItem) {
+            openFragment(new DashBoardFragment());
+        }
+    }
+
+    private void openFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, fragment).addToBackStack(null).commit();
+        getDrawer().closeDrawer(GravityCompat.START);
+    }
 }
--- app/src/main/java/com/example/ClinicManager/ui/fragment/appointment_list/AppointmentListFragment.java
+++ app/src/main/java/com/example/ClinicManager/ui/fragment/appointment_list/AppointmentListFragment.java
@@ -2,17 +2,26 @@ package com.example.ClinicManager.ui.fragment.appointment_list;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.CalendarContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.ClinicManager.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 public class AppointmentListFragment extends Fragment {
@@ -26,7 +35,52 @@ public class AppointmentListFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.appointment_list_fragment, container, false);
+
+        View view = inflater.inflate(R.layout.appointment_list_fragment, container, false);
+
+      /*  Date c = Calendar.getInstance().getTime();
+
+        ContentResolver cr = getContext().getContentResolver();
+        ContentValues values = new ContentValues();
+
+        values.put(CalendarContract.Events.DTSTART, c.getTime());
+        values.put(CalendarContract.Events.TITLE, "title");
+        values.put(CalendarContract.Events.DESCRIPTION, "comment");
+
+        TimeZone timeZone = TimeZone.getDefault();
+        values.put(CalendarContract.Events.EVENT_TIMEZONE, timeZone.getID());
+
+// Default calendar
+        values.put(CalendarContract.Events.CALENDAR_ID, 1);
+
+        //values.put(CalendarContract.Events.RRULE, "FREQ=DAILY;UNTIL=" + dtUntill);
+// Set Period for 1 Hour
+        values.put(CalendarContract.Events.DURATION, "+P1H");
+
+        values.put(CalendarContract.Events.HAS_ALARM, 1);
+
+        SimpleDateFormat yyyymmdd = new SimpleDateFormat("yyyymmdd");
+        Calendar dt = view.findViewById(R.id.calendar);
+
+                // where untilDate is a date instance of your choice,for example 30/01/2012
+                dt.setTime(dt.getTime());
+
+
+        // if you want the event until 30/01/2012 we add one day from our day
+        // because UNTIL in RRule sets events Before the last day want for event
+        dt.add(Calendar.DATE, 1);
+        String dtUntill = yyyymmdd.format(dt.getTime());
+
+        Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI, values);
+
+       *//* // get the event ID that is the last element in the Uri
+        long eventID = Long.parseLong(uri.getLastPathSegment());
+        // add 10 minute reminder for the event
+        ContentValues reminders = new ContentValues();
+        reminders.put(CalendarContract.Reminders.EVENT_ID, eventID);
+        reminders.put(CalendarContract.Reminders.METHOD, CalendarContract.Reminders.METHOD_ALERT);
+        reminders.put(CalendarContract.Reminders.MINUTES, 10);*/
+        return view;
     }
 
     @Override
@@ -34,6 +88,19 @@ public class AppointmentListFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(AppointmentListViewModel.class);
         // TODO: Use the ViewModel
+
     }
 
+    public void addEvent() {
+        Date c = Calendar.getInstance().getTime();
+        ContentResolver cr = getContext().getContentResolver();
+        ContentValues eventValues = new ContentValues();
+        eventValues.put(CalendarContract.Events.TITLE, "title");
+        eventValues.put(CalendarContract.Events.EVENT_LOCATION, "location");
+        eventValues.put(CalendarContract.Events.DTSTART, c.getTime());
+        eventValues.put(CalendarContract.Events.DTEND, c.getTime());
+        eventValues.put(CalendarContract.Events.CALENDAR_ID, "1");//Defaul calendar
+        eventValues.put(CalendarContract.Events.EVENT_TIMEZONE, TimeZone.SHORT);
+        cr.insert(CalendarContract.Events.CONTENT_URI, eventValues);
+    }
 }
