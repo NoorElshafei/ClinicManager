59f33952ea3edda2d1d8afb90df3a42feba6fa4d
Noor Elshafei <noor.elshafie@gmail.com>
2020-09-06 18:05:28 +0200

    design update 5

--- app/src/main/java/com/example/ClinicManager/ui/fragment/dashboard/DashBoardFragment.java
+++ app/src/main/java/com/example/ClinicManager/ui/fragment/dashboard/DashBoardFragment.java
@@ -2,6 +2,7 @@ package com.example.ClinicManager.ui.fragment.dashboard;
 
 import androidx.lifecycle.ViewModelProviders;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,13 +12,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.ClinicManager.R;
+import com.example.ClinicManager.ui.activity.new_schedule.NewScheduleActivity;
 
 
 public class DashBoardFragment extends Fragment {
 
     private DashBoardViewModel mViewModel;
+    private ImageView addAppointment;
 
     public static DashBoardFragment newInstance() {
         return new DashBoardFragment();
@@ -26,7 +30,9 @@ public class DashBoardFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.dash_board_fragment, container, false);
+        View view = inflater.inflate(R.layout.dash_board_fragment, container, false);
+        addAppointment = view.findViewById(R.id.add_appointment);
+        return view;
     }
 
     @Override
@@ -34,6 +40,15 @@ public class DashBoardFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(DashBoardViewModel.class);
         // TODO: Use the ViewModel
+
+        addAppointment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NewScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
