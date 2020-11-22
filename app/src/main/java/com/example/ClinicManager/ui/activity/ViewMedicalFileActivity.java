6a7dd3e67f7438bde5457064a2febab93a5f48cc
Noor Elshafei <noor.elshafie@gmail.com>
2020-11-22 15:06:46 +0200

    Medical history done

---
+++ app/src/main/java/com/example/ClinicManager/ui/activity/ViewMedicalFileActivity.java
@@ -0,0 +1,18 @@
+package com.example.ClinicManager.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ClinicManager.R;
+
+public class ViewMedicalFileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_medical_file);
+
+
+    }
+}
--- app/src/main/java/com/example/ClinicManager/ui/activity/medical_history/MedicalHistoryActivity.java
+++ app/src/main/java/com/example/ClinicManager/ui/activity/medical_history/MedicalHistoryActivity.java
@@ -1,18 +1,22 @@
 package com.example.ClinicManager.ui.activity.medical_history;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.ClinicManager.R;
 import com.example.ClinicManager.ui.adapter.DrugAdapter;
 import com.example.ClinicManager.ui.adapter.MedicalHistoryAdapter;
 
-public class MedicalHistoryActivity extends AppCompatActivity {
+public class MedicalHistoryActivity extends AppCompatActivity implements View.OnClickListener {
     private RecyclerView medialHistoryRecycler;
     private MedicalHistoryAdapter medicalHistoryAdapter;
+    private CardView addMedical;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,21 @@ public class MedicalHistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_medical_history);
 
         medialHistoryRecycler = findViewById(R.id.medical_history_recycler);
+        addMedical = findViewById(R.id.add_medical_button);
+
+        addMedical.setOnClickListener(this);
 
         medialHistoryRecycler.setLayoutManager(new LinearLayoutManager(this,RecyclerView.VERTICAL,false));
 
         medicalHistoryAdapter = new MedicalHistoryAdapter(this);
 
         medialHistoryRecycler.setAdapter(medicalHistoryAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == addMedical){
+            startActivity(new Intent(this,AddHistoryActivity.class));
+        }
     }
 }
